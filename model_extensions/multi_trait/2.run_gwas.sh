8f74f27afdecc5126b3ef65f3dcd6904ed8deccf
filippob <filippo.biscarini@gmail.com>
2026-05-19 09:59:14 +0200

    updated: command lines

--- model_extensions/multi_trait/2.run_gwas.sh
+++ model_extensions/multi_trait/2.run_gwas.sh
@@ -10,5 +10,6 @@ traits='SL,SW'
 covariates='population'
 
 ## stand-alone script
+# Rscript --vanilla gwas_sommer_multitrait_single_snp.R genotype_file=$genotypef snp_map=$snpf phenotype_file=$phenotypef traits=$traits covariates=$covariates thin=200
 Rscript --vanilla gwas_sommer_multitrait_gblup.R genotype_file=$genotypef snp_map=$snpf phenotype_file=$phenotypef traits=$traits covariates=$covariates
 
