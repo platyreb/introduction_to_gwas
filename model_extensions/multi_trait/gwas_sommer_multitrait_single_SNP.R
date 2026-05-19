cf1d1619dc7d76140d0950f206399785125e1d6b
filippob <filippo.biscarini@gmail.com>
2026-05-19 09:50:35 +0200

    added: script to run multi-trait GWAS with a single SNP model

---
+++ model_extensions/multi_trait/gwas_sommer_multitrait_single_SNP.R
@@ -0,0 +1,217 @@
+## R script to carry out a GWAS analysis with the package rrBLUP
+## kinship matrix used to account for population structure in the data
+## input: Plink .raw and .map files + phenotype file
+# run as Rscript --vanilla gwas_sommer.R genotype_file=path_to_genotypes snp_map=path_to_map phenotype_file=path_to_phenotypes trait=trait_name_in_phenotype_file trait_label=label_to_use_for_trait
+
+library("tidyr")
+library("qqman")
+library("dplyr")
+library("sommer")
+library("parallel")
+# library("tidyverse")
+library("data.table")
+
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+
+print("GWAS using the sommer package")
+
+###################################
+## read arguments from command line
+###################################
+allowed_parameters = c(
+  'genotype_file',
+  'snp_map',
+  'phenotype_file',
+  'traits',
+  'covariates',
+  'thin'
+)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+print(args)
+for (p in args){
+  pieces = strsplit(p, '=')[[1]]
+  #sanity check for something=somethingElse
+  if (length(pieces) != 2){
+    stop(paste('badly formatted parameter:', p))
+  }
+  if (pieces[1] %in% allowed_parameters)  {
+    assign(pieces[1], pieces[2])
+    next
+  }
+
+  #if we get here, is an unknown parameter
+  stop(paste('bad parameter:', pieces[1]))
+}
+
+# genotype_file = "introduction_to_gwas/6.steps/rice_imputed.raw"
+# snp_map = "introduction_to_gwas/6.steps/rice_imputed.map"
+# phenotype_file = "introduction_to_gwas/model_extensions/multi_trait/rice_phenotypes_multi.txt"
+# traits = "SL,SW"
+# covariates="population"
+
+print(paste("genotype file name:",genotype_file))
+print(paste("SNP map:",snp_map))
+print(paste("phenotype file name:",phenotype_file))
+print(paste("trait:",traits))
+covariates = if(exists(x = "covariates")) covariates else 1
+print(paste("covariates:",covariates))
+thin = if(exists(x = "thin")) thin else 100
+print(paste("thin:",thin))
+
+dataset = basename(genotype_file)
+
+## READING DATA
+print("now reading in the data ...")
+### genotypes
+snp_matrix <- fread(genotype_file, header = TRUE)
+print(paste(nrow(snp_matrix),"records read from the genotype file",sep=" "))
+SNP_INFO <- fread(snp_map)
+names(SNP_INFO) <- c("Chrom","snp","cM","Position")
+SNP_INFO$cM <- NULL
+
+X <- as.matrix(snp_matrix[,-c(1:6)])
+colnames(X) <- gsub("\\_[A-Z]{1}$","",colnames(X))
+rownames(X) <- snp_matrix$IID
+
+# SNP_INFO <- bind_cols(SNP_INFO,as.data.frame(t(X)))
+
+print(paste(nrow(SNP_INFO),"SNPs read from the map file",sep=" "))
+
+if ((ncol(snp_matrix)-6) != nrow(SNP_INFO)) {
+
+  stop("!! N. of SNPs in the map file not equal to the number of genotyped SNPs in the genotype file")
+
+} else print("N. of SNPs in the map and genotype files is the same: this is correct!!")
+
+### phenotypes
+phenotypes <- fread(phenotype_file)
+# phenotypes <- phenotypes[,c(1,3)]
+print(paste(nrow(phenotypes),"records read from the phenotype file",sep=" "))
+
+phenotypes <- phenotypes[phenotypes$id %in% snp_matrix$IID,]
+print(paste(nrow(phenotypes),"records read from the phenotype file after alignment with genotypes",sep=" "))
+
+## kinship matrix
+print("Calculating the kinship matrix")
+K <-A.mat(X-1)
+
+vec <- colnames(K) %in% phenotypes$id
+K <- K[vec,vec]
+
+# SNP_INFO <- as.data.frame(SNP_INFO)
+# SNP_INFO <- SNP_INFO[,c(TRUE,TRUE,TRUE,vec)]
+
+print("producing the heatmap kinship matrix ...")
+pdf(paste(dataset,"_kinship_heatmap",".pdf",sep=""))
+heatmap(K)
+dev.off()
+
+#### THINNING
+vec = sample(ncol(X), thin)
+X = X[,vec]
+SNP_INFO = SNP_INFO[vec,]
+
+###################
+## Running the GWAS
+###################
+writeLines("- running GWAS for multiple traits")
+# subset phenotypes
+# P <- phenotypes %>% dplyr::rename(phenotype = !!as.name(config$trait))
+trts = strsplit(traits, split = ",")[[1]]
+covs = covariates
+P <- dplyr::select(phenotypes, c(id, all_of(trts), all_of(covs)))
+
+## center and scale
+P <- mutate(P, across(all_of(trts), ~ scale(.) %>% as.vector()))
+
+# pheno <- stackTrait(P, traits = c("SL","SW"))
+# P, X, K, SNP_INFO (map)
+
+run_mt_gwas <- function(snp_name){
+  
+  P$SNP <- X[, snp_name]
+  
+  fit <- mmer(
+    cbind(SL, SW) ~ SNP,
+    random = ~ vsr(id, Gu = K),
+    rcov = ~ vsr(units),
+    data = P,
+    verbose = FALSE
+  )
+  
+  # Extract fixed-effect table
+  sm <- summary(fit)
+  
+  # SNP effect rows
+  beta_table <- sm$betas
+  
+  # keep SNP rows
+  snp_rows <- grep("SNP", beta_table$Effect)
+  
+  tstats <- beta_table[snp_rows, "t.value"]
+  n = nrow(P)
+  pvals = 2*pt(q = tstats, df = n-2, lower.tail = FALSE)
+  
+  # multivariate summary statistic
+  minp <- min(pvals, na.rm = TRUE)
+  
+  return(data.frame(
+    SNP = snp_name,
+    trait = unique(beta_table$Trait),
+    p = pvals
+  ))
+}
+
+results <- mclapply(
+  colnames(X),
+  run_mt_gwas,
+  mc.cores = 8
+)
+
+results <- rbindlist(results)
+results <- results |> inner_join(SNP_INFO, by = c("SNP" = "snp"))
+
+## TRAIT 1
+temp <- filter(results, trait == "SL")
+
+png(paste(dataset,"manhattan_SL.png",sep="_"))
+qqman::manhattan(
+  temp,
+  chr = "Chrom",
+  bp = "Position",
+  snp = "SNP",
+  p = "p",
+  genomewideline = -log10(5e-8),
+  suggestiveline = -log10(1e-5)
+)
+dev.off()
+
+png(paste(dataset,"qqplot_SL.png",sep="_"), width = 600, height = 600)
+qqman::qq(temp$p)
+dev.off()
+
+
+## TRAIT 2
+temp <- filter(results, trait == "SW")
+
+png(paste(dataset,"manhattan_SW.png",sep="_"))
+qqman::manhattan(
+  temp,
+  chr = "Chrom",
+  bp = "Position",
+  snp = "SNP",
+  p = "p",
+  genomewideline = -log10(5e-8),
+  suggestiveline = -log10(1e-5)
+)
+dev.off()
+
+png(paste(dataset,"qqplot_SW.png",sep="_"), width = 600, height = 600)
+qqman::qq(temp$p)
+dev.off()
+
+print("#########")
+print("## END ##")
+print("#########")
